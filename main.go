691696687582ed0a1d0dd038ca3c87563b396cd0
Frederico Barnard Ferreira <frederico.ferreira@b2wdigital.com>
2017-11-28 16:50:54 -0200

    Improving the code, preparing to output to Redis/Elasticsearch

--- main.go
+++ main.go
@@ -1,41 +1,55 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"time"
 
-	"github.com/bred86/redisMonitor/auxRedis"
-	"github.com/bred86/redisMonitor/auxSystem"
+	"github.com/bred86/redisMonitor/aux"
 	"github.com/go-redis/redis"
 )
 
 func main() {
-	var db int
-	var usedMemory int
-	var totalMemory int
-
-	var jsonString string
-	var hostname string
 	var addr string
-	var port string
-	var passwd string
-	var localIP string
-
+	var buffer bytes.Buffer
 	var client *redis.Client
+	var config aux.Config
+	var db int
+	var hostname string
+	var listKeys string
+	var localIP string
+	var passwd string
+	var port string
+	var totalMemory int
+	var usedMemory int
 
-	client = auxRedis.ConnRedis(addr, port, passwd, db)
+	client = aux.ConnRedis(addr, port, passwd, db)
+	config = aux.ReadConfigFile("config/config.json")
+	if config.FromRedis == nil {
+		panic("Needs Redis params to read from")
+	}
 
 	for {
 		now := time.Now()
 		_, _, sec := now.Clock()
 
-		if sec == 10 {
-			usedMemory = auxRedis.GetUsedMemory(client)
-			totalMemory = auxRedis.GetTotalMemory(client)
-			jsonString = auxRedis.GetKeyList(client)
-			hostname = auxSystem.GetHostname()
-			localIP = auxSystem.GetLocalIP()
-			fmt.Printf("{ \"application\": \"redis_monitor\", \"slave_ip\": \"%s\", \"hostname\": \"%s\", %s\"usedMemory\": %d, \"totalMemory\": %d }\n", localIP, hostname, jsonString, usedMemory, totalMemory)
+		if (sec % 10) == 0 {
+			usedMemory = aux.GetUsedMemory(client)
+			totalMemory = aux.GetTotalMemory(client)
+			listKeys = aux.GetKeyList(client)
+			hostname = aux.GetHostname()
+			localIP = aux.GetLocalIP()
+
+			buffer.WriteString(fmt.Sprintf("{\"date\":\"%s\",", now.Format(time.RFC3339)))
+			buffer.WriteString("\"application\":\"redis_monitor\",")
+			buffer.WriteString(fmt.Sprintf("\"slave_ip\":\"%s\",", localIP))
+			buffer.WriteString(fmt.Sprintf("\"hostname\":\"%s\",", hostname))
+			buffer.WriteString(fmt.Sprintf("%s", listKeys))
+			buffer.WriteString(fmt.Sprintf("\"usedMemory\":%d,", usedMemory))
+			buffer.WriteString(fmt.Sprintf("\"totalMemory\":%d}", totalMemory))
+
+			fmt.Println(buffer.String())
 		}
+		time.Sleep(1 * time.Second)
 	}
 }
